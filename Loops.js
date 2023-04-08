221f2a25fd37b9fcc5ed9939a755622e078b882c
Lucas Hemanuel Gomes <lucashemanuel890@gmail.com>
2023-04-08 20:49:48 -0300

    loops chapter finished

---
+++ Loops.js
@@ -0,0 +1,22 @@
+//FOR
+
+for (let index = 0; index <= 10; index++) {
+  console.log(index);
+}
+
+//WHILE
+let loop = 0;
+
+while (loop <= 10) {
+  console.log(loop);
+  loop++;
+}
+
+// DO WHILE
+let i = 0;
+
+do {
+  //i++ RETURNS 1 to 11
+  console.log(i);
+  i++; //0 to 10
+} while (i <= 10);
