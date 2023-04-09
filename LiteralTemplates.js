5fbdaa58a46ceeb3fc3b22970db5c716dd32f076
Lucas Hemanuel Gomes <lucashemanuel890@gmail.com>
2023-04-09 09:42:02 -0300

    literal template string chapter finished

---
+++ LiteralTemplates.js
@@ -0,0 +1,10 @@
+book = {
+  name: "Learning JavaScript Algorithms and DataStructures",
+};
+
+console.log("You are reading " + book.name + "., \n and this is a new line.");
+
+//Template Literal Strings always inside (``)
+console.log(`You are reading ${book.name}.,
+  and this is a new line.
+`);
