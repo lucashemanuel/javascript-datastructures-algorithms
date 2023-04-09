81d3b7730bf416496635df31762f1634ef18b7e9
Lucas Hemanuel Gomes <lucashemanuel890@gmail.com>
2023-04-09 20:09:38 -0300

    exponential operator chapter finished

---
+++ ExponentialOperator.js
@@ -0,0 +1,13 @@
+var side = 2;
+
+const circleArea = 3.14 * side * side;
+
+const area = 3.14 * side ** 2;
+
+const area1 = Math.PI * Math.pow(side, 2);
+
+console.log(circleArea); //output 12.56
+console.log(area); //output 12.56
+console.log(area1); //output 12.566370614359172
+
+//That's all the same
